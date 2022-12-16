95265f074c792d3f30a190bf641eda215bb04f3a
Bladimiro <bladimir.calcina@gmail.com>
2022-12-15 22:45:59 -0400

    fastify library added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // express
+  //const app = await NestFactory.create(AppModule);
+
+  // fastify
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Todo example')
